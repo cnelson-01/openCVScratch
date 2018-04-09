3e1c09eaa64f9e3687af0e8ec1040e7e0be9b63f
Chris <cnelson1200@gmail.com>
2018-04-08 21:07:06 -0700

    added video capture on threshold trigger

--- main.cpp
+++ main.cpp
@@ -1,3 +1,7 @@
+#include <chrono>
+#include <thread>
+#include <iostream>
+
 #include <opencv2/core.hpp>
 #include <opencv2/videoio.hpp>
 #include <opencv2/highgui.hpp>
@@ -5,8 +9,19 @@
 #include <stdio.h>
 #include <opencv2/imgproc.hpp>
 
+
+// There are other clocks, but this is usually the one you want.
+// It corresponds to CLOCK_MONOTONIC at the syscall level.
+using Clock = std::chrono::steady_clock;
+using std::chrono::time_point;
+using std::chrono::duration_cast;
+using std::chrono::milliseconds;
+//using namespace std::literals::chrono_literals;
+using std::this_thread::sleep_for;
+
 using namespace cv;
 using namespace std;
+
 
 int main(int, char **) {
     Mat frame;
@@ -16,6 +31,9 @@ int main(int, char **) {
     cvInitFont(&font, 1, 1, 1);
     double sum = 0;
     uint32_t thresh = 1000000;
+
+    uint32_t numFrames = 10;
+    Mat capture[numFrames];
     //--- INITIALIZE VIDEOCAPTURE
     VideoCapture cap;
     // open the default camera using default API
@@ -42,15 +60,28 @@ int main(int, char **) {
             cerr << "ERROR! blank frame grabbed\n";
             break;
         }
+
+        time_point<Clock> start = Clock::now();
         res = keyFrame - frame;
         sum = cv::sum(res)[0];
-        putText(res, to_string(sum), cvPoint(100, 100), 1, 1, 255);
-        // show live and wait for a key with timeout long enough to show images
+
         if (sum > thresh) {
-            imshow("Live", 255 - res);
+//            imshow("Live", cv::Scalar(255,255,255) - res);
+            imshow("Live", res);
+            // Grab a few frames
+            for (int i = 0; i < numFrames; i++) {
+                cap.read(capture[i]);
+            }
+            time_point<Clock> end = Clock::now();
+            milliseconds diff = duration_cast<milliseconds>(end - start);
+            std::cout << diff.count() << "ms" << " " << ((double) numFrames * 1000) / diff.count() << std::endl;
         } else {
             imshow("Live", res);
+            time_point<Clock> end = Clock::now();
+            milliseconds diff = duration_cast<milliseconds>(end - start);
+            std::cout << diff.count() << "ms" << " " << ((double) 1) / diff.count() << std::endl;
         }
+
         if (waitKey(5) >= 0)
             break;
     }
